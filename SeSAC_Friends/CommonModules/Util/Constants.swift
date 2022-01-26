13bf680d5ed24a5fa0f930b3b10b8f7e0c713b1c
AlexHwang <cksgh0316@gmail.com>
2022-01-26 10:53:22 +0900

    01/26

---
+++ SeSAC_Friends/CommonModules/Util/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  SeSAC_Friends
+//
+//  Created by AlexHwang on 2022/01/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let isFirstLoggin = "firstLogin"
+    
+    struct UserInfo {
+        static let firebaseAuth = "authVerificationID"
+        static let userPhoneNum = "userPhoneNumber"
+        
+        
+        static let token = "userToken"
+        static let userName = "username"
+        static let userId = "userId"
+        static let userEmail = "userEmail"
+        static let password = "userPassword"
+    }
+}
